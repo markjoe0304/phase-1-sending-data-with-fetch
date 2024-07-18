fa1ec81ac082e66d440fe0b7fb72080c603d05ed
markjoe0304 <markjoegicheru@gmail.com>
2024-07-18 09:43:57 +0300

    moded

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newItem = data.id;
+        const newElement = document.createElement("p");
+        newElement.textContent = newItem;
+        document.body.appendChild(newElement);
+      })
+  
+      .catch((error) => {
+        const newElement = document.createElement("p");
+        newElement.textContent = `Error: ${error.message}`;
+        document.body.appendChild(newElement);
+      });
+  }
+  
